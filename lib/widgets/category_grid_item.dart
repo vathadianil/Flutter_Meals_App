72febf4efb44aff31e2738246167ec01838a62ce
Anil <anil.kumar@shellinfoglobal.com>
2024-10-05 21:07:41 +0530

    Category Grid Item added

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  final Category category;
+  const CategoryGridItem({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(.9),
+          ],
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge!
+            .copyWith(color: Theme.of(context).colorScheme.onPrimaryContainer),
+      ),
+    );
+  }
+}
