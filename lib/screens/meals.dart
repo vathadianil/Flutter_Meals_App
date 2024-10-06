695e30c1587443ee2d16cb7eaee7a846c1b87b53
Anil <anil.kumar@shellinfoglobal.com>
2024-10-06 10:44:07 +0530

    Stack widget to place items one another

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -34,7 +35,7 @@ class MealsScreen extends StatelessWidget {
       content = ListView.builder(
         itemCount: meals.length,
         itemBuilder: (ctx, index) {
-          return Text(meals[index].title);
+          return MealItem(meal: meals[index]);
         },
       );
     }
